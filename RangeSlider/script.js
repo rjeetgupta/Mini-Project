ac9bc06d496662675fd87a7fc84c094c011bd581
rjeetgupta <ranjeeturd@gmail.com>
2024-02-20 20:04:40 +0530

    Html and CSS part has been done

---
+++ RangeSlider/script.js
@@ -0,0 +1,6 @@
+const minRange = document.getElementById('min-range');
+const maxRange = document.getElementById('max-range');
+const priceRange = document.getElementById('price-range');
+
+for (let i = 0; i < priceRange.length; i++) {
+    
