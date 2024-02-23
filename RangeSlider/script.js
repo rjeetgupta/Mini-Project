90d49d4b39394cc5ee29ccc2245ba94e7ac3a668
rjeetgupta <ranjeeturd@gmail.com>
2024-02-23 23:57:41 +0530

    Add, Edit and Delete functionality added

--- RangeSlider/script.js
+++ RangeSlider/script.js
@@ -3,4 +3,5 @@ const maxRange = document.getElementById('max-range');
 const priceRange = document.getElementById('price-range');
 
 for (let i = 0; i < priceRange.length; i++) {
-    
+   
+}
--- ToDo/script.js
+++ ToDo/script.js
@@ -2,63 +2,58 @@ const addButton = document.getElementById("btn");
 const inputValue = document.getElementById('inputValue');
 const listItem = document.getElementById('listItem');
 
-
-
 function addTask() {
     const taskText = inputValue.value.trim();
-
     if (taskText === '') {
         alert('Please enter your task');
         return;
     }
+    const taskItem = createTaskItem(taskText);
+    listItem.appendChild(taskItem);
+    clearInputValue();
+}
+
+function createTaskItem(taskText) {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
     
+    const taskTextNode = document.createTextNode(taskText);
+    
+    const editButton = createButton('Edit');
+    editButton.onclick = function() {
+        editTask(taskTextNode);
+    };
+
+    const deleteButton = createButton('Delete');
+    deleteButton.onclick = function() {
+        listItem.removeChild(li);
+    };
+
     const li = document.createElement('li');
-    li.textContent = addTask;
-    listItem.appendChild('li')
-    addTask(`${taskText}`)
-    
-    // Clear the input value after adding task
+    li.appendChild(checkbox);
+    li.appendChild(taskTextNode);
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+
+    return li;
+}
+
+function createButton(text) {
+    const button = document.createElement('button');
+    button.innerText = text;
+    button.className = text.toLowerCase() + '-button';
+    return button;
+}
+
+function editTask(taskTextNode) {
+    const newText = prompt('Enter new task text:', taskTextNode.textContent);
+    if (newText !== null) {
+        taskTextNode.textContent = newText;
+    }
+}
+
+function clearInputValue() {
     inputValue.value = '';
 }
 
-addButton.addEventListener('click', addTask);
-
-
-// const button = document.querySelector('.btn');
-// // const toDoBox = document.querySelector('todo-box').value;
-// const inputValue = document.getElementById('inputValue');
-
-// button.addEventListener('click', addTask() )
-
-//     function addTask() {
-//         const taskText = inputValue.value.trim();
-//         if (taskText === ' ') {
-//             alert('Please enter a task');
-//             return;
-//         }
-//         const li = document.createElement('li');
-//         li.appendChild(document.createTextNode(taskText));
-//         document.querySelector('.todo-box').appendChild(li);
-//         // document.body.appendChild(li);
-//     }
-    // createList(`${inputValue}`);
-    
-    
-    // console.log(inputValue);
-    // alert("button clicked");
-
-
-
-// const addToDo = (li) => {
-//     const listItem = document.createElement('li');
-//     listItem.innerHTML = `
-//         ${inputValue}
-//         <i class="fas fa-times"></i>
-//     `;
-//     toDoBox.appendChild(listItem);
-// }
-
-// const removeElements = document.getElementsByClassName('fa-times')
-// removeElements.addEventListener('click', function () {
-//     li.remove();
-// })
+addButton.addEventListener("click", addTask);
